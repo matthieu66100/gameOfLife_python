b2e1a60697719acf254fcf050335fedf2bb8e1d8
Basto Matthieu <matthieu.basto@gmail.com>
2024-10-21 22:48:34 +0200

    modifications de grilles

--- game.py
+++ game.py
@@ -4,121 +4,96 @@ import random
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen_size = (1000,1000)
+        self.screen_size = (1000, 1000)
         self.screen = pygame.display.set_mode(self.screen_size)
-        self.clock = pygame.time.Clock()
-        pygame.display.set_caption('Game Of Life')
+        pygame.display.set_caption('Jeu de la Vie')
+        self.cell_size = 10
+        self.grid_dims = (self.screen_size[0] // self.cell_size, self.screen_size[1] // self.cell_size)
+        self.grid = [[0] * self.grid_dims[0] for _ in range(self.grid_dims[1])]
         self.running = True
         self.play = False
-        self.sizecell = 5 #size in pixels
-        self.FPS = 120
+        self.offset = [0, 0]
+        self.mouse_down = False
+        self.show_grid = True  #contrôler l'affichage de la grille
 
-        self.init_tabs()
-        self.main()
+    def draw_grid(self):
+        for i, row in enumerate(self.grid):
+            for j, cell in enumerate(row):
+                x = (j * self.cell_size) + self.offset[0]
+                y = (i * self.cell_size) + self.offset[1]
+                if 0 <= x < self.screen_size[0] and 0 <= y < self.screen_size[1]:
+                    if cell:
+                        pygame.draw.rect(self.screen, (60, 60, 150), (x, y, self.cell_size, self.cell_size))
+                    elif self.show_grid:
+                        pygame.draw.rect(self.screen, (220, 220, 220), (x, y, self.cell_size, self.cell_size), 1)
 
-    # Initialise all the tables and 
-    def init_tabs(self):
-        self.tab = (int(self.screen_size[0] / self.sizecell),int(self.screen_size[1]/self.sizecell)) #table template for all other tables
-        self.tab_1      = [[0]*self.tab[0] for i in range(self.tab[1])] #tab_1 is actual table draw
-        self.tab_2      = [[0]*self.tab[0] for i in range(self.tab[1])] #tab_2 is the futur generation
-        self.tab_check  = [[0]*self.tab[0] for i in range(self.tab[1])] #tab_check is use to calculate valor of life
+    def update_grid(self):
+        new_grid = [[0] * self.grid_dims[0] for _ in range(self.grid_dims[1])]
+        for i in range(self.grid_dims[1]):
+            for j in range(self.grid_dims[0]):
+                neighbors = sum(self.grid[x][y] for x in range(max(0, i-1), min(self.grid_dims[1], i+2))
+                                              for y in range(max(0, j-1), min(self.grid_dims[0], j+2))
+                                              if (x, y) != (i, j))
+                new_grid[i][j] = neighbors == 3 or (neighbors == 2 and self.grid[i][j])
+        self.grid = new_grid
 
-    def printTable(self):
-        for i in range(self.tab[0]):
-            for j in range(self.tab[1]):
-                if self.tab_1[i][j] == 1:
-                    self.draw(i,j)
-
-    def addCell(self, x, y):
-        self.tab_1[x][y] = 1
-
-    def randGenerate(self):
-        for i in range(self.tab[0]):
-            for j in range(self.tab[1]):
-                self.tab_1[i][j] = random.randint(0,1)
-
-    def tampTab(self):
-        for i in range(self.tab[0]):
-            for j in range(self.tab[1]):
-                self.tab_1[i][j] = self.tab_2[i][j]
-
-    def draw(self, x,y):
-        pygame.draw.rect(self.screen, (60,60,150),(x*self.sizecell, y*self.sizecell, self.sizecell, self.sizecell))
-
-    def newGeneration(self):
-        for i in range(self.tab[0]):
-            for j in range(self.tab[1]):
-                self.tab_check[i][j] = self.arroundCheck(i,j)
-                
-                #si 3 voisine = vivante
-                if self.tab_check[i][j] == 3:
-                    self.tab_2[i][j] = 1
-                
-                #si <2 voisines = morte
-                elif self.tab_check[i][j] <2:
-                    self.tab_2[i][j] = 0
-                
-                #si >3 voisines = morte
-                elif(self.tab_check[i][j] >3):
-                    self.tab_2[i][j] = 0
-                
-                #si 2 voisines = bouge pas
-                else:
-                    self.tab_2[i][j] = self.tab_1[i][j]
-
-    def arroundCheck(self,x,y):
-        value = 0
-        if x > 0 and y > 0 and self.tab_1[x-1][y-1] == 1 : value = value + 1
-        if y > 0 and self.tab_1[x][y-1] == 1: value = value + 1
-        if x < self.tab[0]-1 and y > 0 and self.tab_1[x+1][y-1] == 1: value = value + 1
-     
-        if x > 0 and self.tab_1[x-1][y] == 1 : value = value + 1
-        if x < self.tab[0]-1 and self.tab_1[x+1][y] == 1 : value = value + 1
-     
-        if x > 0 and y < self.tab[1]-1 and self.tab_1[x-1][y+1] == 1 : value = value + 1
-        if y < self.tab[1]-1 and self.tab_1[x][y+1] == 1 : value = value + 1
-        if x < self.tab[0]-1 and y < self.tab[1]-1 and self.tab_1[x+1][y+1] == 1 : value = value + 1
-        
-        return value 
-    
-
-    def events(self):
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 self.running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.running = False
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
-                    self.randGenerate()
-                    print("random generation")
-                if event.key == pygame.K_SPACE:
-                    if self.play == False:
-                        self.play = True
-                        print("play")
-                    elif self.play == True:
-                        self.play = False
-                        print("pause")
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                posX = int(pygame.mouse.get_pos()[0]/self.sizecell)
-                posY = int(pygame.mouse.get_pos()[1]/self.sizecell)
-                print(posX,posY)
-                self.addCell(posX,posY)
-    
-    def loop(self):
+                    self.grid = [[random.randint(0, 1) for _ in range(self.grid_dims[0])] for _ in range(self.grid_dims[1])]
+                elif event.key == pygame.K_SPACE:
+                    self.play = not self.play
+                elif event.key == pygame.K_g:  #afficher/cacher la grille
+                    self.show_grid = not self.show_grid
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Clic gauche
+                    self.mouse_down = True
+                    self.place_cell()
+                elif event.button == 4:  # Molette vers le haut
+                    self.zoom_in()
+                elif event.button == 5:  # Molette vers le bas
+                    self.zoom_out()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:  # Relâchement du clic gauche
+                    self.mouse_down = False
+            elif event.type == pygame.MOUSEMOTION:
+                if self.mouse_down:
+                    self.place_cell()
+
+    def place_cell(self):
+        pos = pygame.mouse.get_pos()
+        grid_x = (pos[0] - self.offset[0]) // self.cell_size
+        grid_y = (pos[1] - self.offset[1]) // self.cell_size
+        if 0 <= grid_x < self.grid_dims[0] and 0 <= grid_y < self.grid_dims[1]:
+            self.grid[grid_y][grid_x] = 1
+
+    def zoom_in(self):
+        if self.cell_size < 50:  # Limite de zoom
+            self.cell_size += 1
+            self.adjust_offset()
+
+    def zoom_out(self):
+        if self.cell_size > 2:  # Limite de dézoom
+            self.cell_size -= 1
+            self.adjust_offset()
+
+    def adjust_offset(self):
+        center_x = (self.screen_size[0] // 2 - self.offset[0]) // self.cell_size
+        center_y = (self.screen_size[1] // 2 - self.offset[1]) // self.cell_size
+        self.offset[0] = self.screen_size[0] // 2 - center_x * self.cell_size
+        self.offset[1] = self.screen_size[1] // 2 - center_y * self.cell_size
+
+    def run(self):
+        clock = pygame.time.Clock()
         while self.running:
-            self.screen.fill((200,200,200))
-            self.printTable()
-    
-            self.events()
-
-            if self.play == True:
-                self.newGeneration()
-                self.tampTab()
-
+            self.screen.fill((200, 200, 200))
+            self.draw_grid()
+            self.handle_events()
+            if self.play:
+                self.update_grid()
             pygame.display.update()
-            self.clock.tick(self.FPS)
-
-    def main(self):
-        self.loop()
+            clock.tick(30)
+        pygame.quit()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from game import Game
 
-
-Game()
+game = Game()
+game.run()
